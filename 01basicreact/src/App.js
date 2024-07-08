0cec956aa98715d8653274d01b4223acad517f68
GAGANDASH002 <gagandash002@gmail.com>
2024-07-08 19:15:25 +0530

    create first react component using CRA

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,17 @@
+// the function / component should be in uppercase,
+// we should also keep the name of file in uppercase
+
+import Test from './Test'
+
+function App() {
+  return (
+    <>
+    <Test />
+    <h1>Hello World</h1>
+    </>
+  );
+}
+
+// This line exports the App component as the default export from this module,
+// making it available to be imported and used in other parts of the application. 
+export default App;
---
+++ 01basicreact/src/Test.js
@@ -0,0 +1,8 @@
+
+function Test(){
+    return(
+        <h2>Hello I am React</h2>
+    )
+}
+
+export default Test

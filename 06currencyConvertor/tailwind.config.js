b1c37b3db20e31c2368e25387fb5a5066591f51b
GAGANDASH002 <gagandash002@gmail.com>
2024-07-12 19:46:24 +0530

    currency convertor using React , custom hooks , useId() hook

---
+++ 06currencyConvertor/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Includes all JS, JSX, TS, and TSX files in the src directory
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
